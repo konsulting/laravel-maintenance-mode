1aa409c7656c9b86c35839b338becd057ab00673
Robin D'Arcy <robin@klever.co.uk>
2018-05-16 23:45:30 +0100

    Add cache driver

---
+++ src/Drivers/CacheDriver.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Konsulting\Laravel\MaintenanceMode\Drivers;
+
+use Cache;
+use Konsulting\Laravel\MaintenanceMode\DownPayload;
+
+class CacheDriver implements DriverInterface
+{
+    public function key()
+    {
+        return 'maintenance.down';
+    }
+
+    /**
+     * Activate maintenance mode.
+     *
+     * @param DownPayload $payload
+     * @return bool
+     */
+    public function activate(DownPayload $payload)
+    {
+        return Cache::forever($this->key(), $payload->toArray());
+    }
+
+    /**
+     * Deactivate maintenance mode.
+     *
+     * @return bool
+     */
+    public function deactivate()
+    {
+        return Cache::forget($this->key());
+    }
+
+    /**
+     * Check if maintenance mode is active.
+     *
+     * @return bool
+     */
+    public function isActive()
+    {
+        return Cache::has($this->key());
+    }
+
+    /**
+     * Get the information specified when the site was taken down.
+     *
+     * @return DownPayload
+     */
+    public function downInformation()
+    {
+        $content = Cache::get($this->key());
+
+        return new DownPayload($content);
+    }
+}
---
+++ tests/Drivers/CacheDriverTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Konsulting\Laravel\MaintenanceMode\Tests\Drivers;
+
+use Cache;
+use Konsulting\Laravel\MaintenanceMode\Drivers\CacheDriver;
+use Konsulting\Laravel\MaintenanceMode\Tests\TestSupport\CacheTableProvider;
+
+class CacheDriverTest extends DriverTestCase
+{
+    protected $driverClass = CacheDriver::class;
+
+    protected function setUp()
+    {
+        parent::setUp();
+    }
+
+    /** @test */
+    public function it_takes_the_site_down()
+    {
+        $this->maintenanceMode->on();
+
+        $this->assertTrue(Cache::has('maintenance.down'));
+        $this->assertTrue($this->maintenanceMode->isOn());
+    }
+
+    /** @test */
+    public function it_brings_the_site_back_up()
+    {
+        $this->setDownPayloadInCache();
+        $this->maintenanceMode->off();
+
+        $this->assertFalse(Cache::has('maintenance.down'));
+        $this->assertFalse($this->maintenanceMode->isOn());
+    }
+
+    protected function setDownPayloadInCache()
+    {
+        Cache::forever('maintenance.down', true);
+
+        $this->assertTrue($this->maintenanceMode->isOn());
+    }
+}
