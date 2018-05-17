71c09aac4664fcebc853eb2fea7f7867b37a35a5
Robin D'Arcy <robin@klever.co.uk>
2018-05-18 00:06:09 +0100

    Update config file

--- config/maintenance_mode.php
+++ config/maintenance_mode.php
@@ -1,5 +1,43 @@
 <?php
 
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Maintenance mode driver
+    |--------------------------------------------------------------------------
+    |
+    | This is the driver used when activating maintenance mode. For simple
+    | single-server configurations, the file driver is sufficient. For
+    | multi-server setups, the cache or storage drivers are available.
+    |
+    | Supported: "file", "storage", "cache"
+    |
+    */
+
     'driver' => 'file',
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Driver configuration
+    |--------------------------------------------------------------------------
+    |
+    | This is the configuration for each driver. All drivers need either
+    | a file path or key specified, which will be the location at which
+    | the down payload is stored.
+    |
+    */
+
+    'file' => [
+        'file_path' => 'maintenance/down',
+    ],
+
+    'storage' => [
+        'file_path' => 'maintenance/down',
+    ],
+
+    'cache' => [
+        'key' => 'maintenance.down',
+    ],
 ];
